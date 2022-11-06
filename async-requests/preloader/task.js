8f252f3da01c784607b3d81f6e3584fe1f7db1ae
dzuba110729 <dzyuba1107@outlook.com>
2022-11-06 12:15:06 +0300

    new commit

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,31 +1,80 @@
-const items = document.getElementById('items');
-const loader = document.getElementById('loader');
+const loader = document.querySelector('#loader');
+const items = document.querySelector('#items');
+const requestUrl = 'https://netology-slow-rest.herokuapp.com';
+let localStorageArray = [];
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/');
-xhr.send();
+function hideLoader() {
+    if (loader.classList.contains('loader_active')) {
+        loader.classList.remove('loader_active');
+    }
+}
 
-xhr.addEventListener('readystatechange', function () {
+function showLoader() {
+    if (!loader.classList.contains('loader_active')) {
+        loader.classList.add('loader_active');
+    }
+}
 
-	if (this.readyState == xhr.DONE && this.status == 200) {
+function cleanValutesList() {
+    while (items.firstChild) {
+        items.removeChild(items.firstChild);
+    };
+}
 
-		loader.classList.remove('loader_active');
-		let data = JSON.parse(this.responseText).response.Valute;
+function makeValuteList(valutes) {
+    cleanValutesList();
+    for (valute in valutes) {
+        makeValute(valutes[valute]);
+    }
+}
 
-		for (var valute in data) {
-			items.innerHTML += `
-        <div class="item">
-            <div class="item__code">
-                ${data[valute].CharCode}
-            </div>
-            <div class="item__value">
-                ${data[valute].Value}
-            </div>
-            <div class="item__currency">
-                руб.
-            </div>
-        </div>
-        `;
-		}
-	}
-});
+function makeValute(valute) {
+    const newEmptyDiv = document.createElement('div');
+    newEmptyDiv.classList.add('item');
+    const valuteDiv = items.appendChild(newEmptyDiv);
+    const currencyHTML = `
+    <div class="item__code">${valute.CharCode}</div>
+    <div class="item__value">${valute.Value}</div>
+    <div class="item__currency">руб.</div>
+    `;
+    valuteDiv.insertAdjacentHTML('afterBegin', currencyHTML);
+
+    //save to LS
+    saveValuteToLS(valute.CharCode, valute.Value);
+}
+
+function makeRequest() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', requestUrl, true);
+    xhr.responseType = 'json';
+    xhr.send();
+    cleanValutesList();
+    showLoader();
+    xhr.addEventListener('readystatechange', () => {
+        responseHandler(xhr);
+    });
+}
+
+function responseHandler(xhr) {
+    if (xhr.readyState === xhr.DONE) {
+        const valutes = xhr.response.response.Valute;
+        makeValuteList(valutes);
+        hideLoader();
+    }
+}
+
+function saveValuteToLS(item, value) {
+    localStorageArray.push({ CharCode: item, Value: value });
+    localStorage.setItem('valutesArray', JSON.stringify(localStorageArray));
+}
+
+function loadValutesFromLS() {
+    if (localStorage.valutesArray) {
+        localStorageArray = JSON.parse(localStorage.valutesArray);
+        localStorageArray.forEach(elem => makeValute(elem));
+    }
+}
+
+loadValutesFromLS();
+
+makeRequest();
